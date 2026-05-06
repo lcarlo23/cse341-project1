d45fad13e297df10250eda0b0429380e256fe5ed
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-05-06 17:35:47 +0200

    Connect db to server

---
+++ src/controllers/databaseController.js
@@ -0,0 +1,33 @@
+import { MongoClient } from 'mongodb';
+import { loadEnvFile } from 'process';
+
+loadEnvFile();
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+let db;
+let collection;
+
+export async function initDb() {
+    try {
+        await client.connect();
+        db = client.db('cse341');
+        collection = db.collection('contacts');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getDb() {
+    if (!db) {
+        throw Error("Database not initialized");
+    }
+
+    return db;
+}
+
+export async function closeDb() {
+    await client.close();
+    console.log('MongoDB connection closed.');
+    process.exit(0);
+}
