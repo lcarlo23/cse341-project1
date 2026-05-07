c40bcc5508046c1fc460627bdbcaf5f7a7a79dda
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-05-07 12:22:45 +0200

    Fix error message

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -9,7 +9,7 @@ export async function getAllContacts(req, res) {
 
     res.status(200).json(contacts);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error.message);
   }
 }
 
