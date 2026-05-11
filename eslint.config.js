2bc4fd09e6a710fadbe39af32f7549821c92686f
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-05-11 14:16:20 +0200

    Configure linter and format

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettierConfig from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  js.configs.recommended,
+  prettierConfig,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  },
+]);
